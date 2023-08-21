2e3cbb724fe37ccf6883415d2974b2e674bc1573
Alfredo Hdz <a1ahl611@gmail.com>
2023-08-20 18:58:23 -0600

    Added dark mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   eventListeners();
+  darkMode();
 });
+
+function darkMode() {
+  const darkModePrefer = window.matchMedia('(prefers-color-scheme: dark)');
+
+  if(darkModePrefer.matches) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
+
+  darkModePrefer.addEventListener('change', function(){
+    if(darkModePrefer.matches) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  });
+  
+  const botonDarkMode = document.querySelector('.dark-mode-boton');
+  botonDarkMode.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+  });
+}
 
 function eventListeners() {
   const mobileMenu = document.querySelector('.mobile-menu');
